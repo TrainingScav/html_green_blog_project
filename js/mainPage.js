0ab28f14caa95358abf7067cff23f34ab885ff7d
TrainingScav <dnslwk123@naver.com>
2025-06-15 02:00:27 +0900

    화면 프레임 작업 80%

--- js/mainPage.js
+++ js/mainPage.js
@@ -9,20 +9,38 @@ const pageHeight = Math.max(
   document.documentElement.clientHeight
 );
 
-const modal = document.querySelector('.modal');
-const modalOpen = document.querySelector('.write-post-icon');
-const modalClose = document.querySelector('.close_btn');
+// header 수정 모달 관련 요소 정보
+const header_modal = document.querySelector('.header-modal');
+const header_modalOpen = document.querySelector('.header-icon');
+const header_modalClose = document.querySelector('.header-close_btn');
 
+// 사이드 menu 정보 관련 요소 정보
+const menu_modal = document.querySelector('.menu-modal');
+const menu_modalOpen = document.querySelector('.menu-icon');
+const menu_modalClose = document.querySelector('.menu-close_btn');
+
+
+
+// ================= header 모달 함수 ====================
 //열기 버튼을 눌렀을 때 모달팝업이 열림
-modalOpen.addEventListener('click',function(){
-    //alert("테스트");
-    
-    modal.style.display = 'block';
-    modal.style.height = pageHeight + 'px';
+header_modalOpen.addEventListener('click',function(){
+    header_modal.style.display = 'block';
+    header_modal.style.height = (pageHeight+200) + 'px';
 });
 //닫기 버튼을 눌렀을 때 모달팝업이 닫힘
-modalClose.addEventListener('click',function(){
-    modal.style.display = 'none';
+header_modalClose.addEventListener('click',function(){
+    header_modal.style.display = 'none';
 });
+// ================= header 모달 함수 종료 ====================
 
-// ============== 모달팝업 작업 종료 ==============
+
+// ================= menu 모달 함수 ====================
+menu_modalOpen.addEventListener('click',function(){
+    menu_modal.style.display = 'block';
+    menu_modal.style.height = (pageHeight+200) + 'px';
+});
+//닫기 버튼을 눌렀을 때 모달팝업이 닫힘
+menu_modalClose.addEventListener('click',function(){
+    menu_modal.style.display = 'none';
+});
+// ================= menu 모달 함수 종료 ====================
